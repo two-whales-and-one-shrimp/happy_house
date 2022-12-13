4d6a869e7e1d032b0e669e720908849cebde6fb8
jaehe <jaehe0413@naver.com>
2022-12-14 02:58:54 +0900

    feature : 회원삭제 request type 변경

--- server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/AdminController.java
+++ server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class AdminController {
     return userService.findAll();
   }
 
-  @GetMapping("/{userId}")
+  @DeleteMapping("/{userId}")
   public ResponseEntity<?> deleteUserById(@PathVariable String userId){
     try{
       userService.deleteUserById(userId);
