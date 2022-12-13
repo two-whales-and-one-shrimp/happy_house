b6acc8e79c6e6c089c32e714ee8527508a1f532c
김호균 <H0KyunGim@gmail.com>
2022-12-14 03:29:43 +0900

    Feature/32 로그아웃 (#35)
    * feat: 로그아웃 front-end
    
    * feat: 로그아웃-backend

--- server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/UserController.java
+++ server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,14 @@ public class UserController {
       return new ResponseEntity<>(userSignInResultDto, HttpStatus.OK);
     }
     return new ResponseEntity<>(userSignInResultDto, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @GetMapping("/signout/{userId}")
+  public ResponseEntity<?> signOut(@RequestHeader("X-ACCESS-TOKEN") String token, @PathVariable String userId) {
+    if(userService.signOut(userId, token))
+      return new ResponseEntity<>(HttpStatus.OK);
+    else 
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @GetMapping("/{userId}")
