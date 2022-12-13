466930fbb7210579d33c0dcec9f9137829dc1442
hokyun <h0kyungim@gmail.com>
2022-12-14 01:50:05 +0900

    feat: 로그인 화면 구성 및 rest api 제작

--- client/happyhouse_client/src/components/header/TheHeader.vue
+++ client/happyhouse_client/src/components/header/TheHeader.vue
@@ -4,14 +4,14 @@
     <v-spacer></v-spacer>
     <span class="mx-2">
       <v-btn exact to="/map" text x-large class="mr-5"> 지도 </v-btn>
-      <span v-if="!isSignIn">
+      <span v-if="userId == null">
         <v-btn exact to="/user/sign-in" text x-large class="mr-5">로그인</v-btn>
         <v-btn exact to="/user/sign-up" text x-large class="mr-5">
           회원가입
         </v-btn>
       </span>
       <span v-else>
-        <avartar @signOut="signOut" :userId="user.id" />
+        <avartar @signOut="signOut" :userId="userId" />
       </span>
     </span>
   </div>
@@ -20,24 +20,24 @@
 <script>
 import logo from "@/components/header/Logo";
 import avartar from "@/components/header/Avartar";
+import { mapState } from "vuex";
+
+const userStore = "userStore";
+
 export default {
   name: "TheHeader",
   data() {
-    return {
-      isSignIn: true,
-      user: {
-        id: "testId",
-      },
-    };
+    return {};
   },
   components: {
     logo: logo,
     avartar: avartar,
   },
+  computed: {
+    ...mapState(userStore, ["userId"]),
+  },
   methods: {
-    signOut() {
-      this.isSignIn = false;
-    },
+    signOut() {},
   },
 };
 </script>
---
+++ client/happyhouse_client/src/store/modules/user-store.js
@@ -0,0 +1,59 @@
+import { signIn } from "@/api/user.js";
+
+const userStore = {
+  namespaced: true,
+  state: {
+    isAdmin: sessionStorage.getItem("isAdmin"),
+    userId: sessionStorage.getItem("userId"),
+  },
+  getters: {
+    getUserId: function (state) {
+      return state.userId;
+    },
+  },
+  mutations: {
+    SET_USER_INFO: (state, userInfo) => {
+      state.userId = userInfo.id;
+      state.isAdmin = userInfo.isAdmin;
+      sessionStorage.setItem("userId", userInfo.id);
+      sessionStorage.setItem("isAdmin", userInfo.isAdmin);
+    },
+
+    REMOVE_USER_INFO: (state) => {
+      state.userId = null;
+      state.isAdmin = false;
+      sessionStorage.removeItem("userId");
+      sessionStorage.removeItem("isAdmin");
+    },
+  },
+  actions: {
+    async userSignIn({ commit }, userInfo) {
+      let response = await signIn(userInfo.id, userInfo.password);
+      if (response.status == 200) {
+        commit("SET_USER_INFO", {
+          id: response.data.userId,
+          isAdmin: response.data.isAdmin,
+        });
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    // async userSignOut({ commit }) {
+    //   let response = await signIn(userInfo.id, userInfo.password);
+    //   if (response.status == 200) {
+    //     commit("REMOVE_USER_INFO");
+    //     localStorage.setItem("accessToken", response.data.accessToken);
+    //     localStorage.setItem("refreshToken", response.data.refreshToken);
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // },
+  },
+};
+
+export default userStore;
