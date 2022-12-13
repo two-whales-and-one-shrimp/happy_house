505b21fe38f89d590ca571bd931f4677c1201be4
jaehe <jaehe0413@naver.com>
2022-12-14 02:53:53 +0900

    feature : 회원삭제-server

--- server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/AdminController.java
+++ server/happyhouse_server/src/main/java/com/ssafy/house/user/controller/AdminController.java
@@ -3,7 +3,10 @@ package com.ssafy.house.user.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,16 @@ public class AdminController {
   @GetMapping
   public List<UserListDto> getUserList(){
     return userService.findAll();
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<?> deleteUserById(@PathVariable String userId){
+    try{
+      userService.deleteUserById(userId);
+      return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    }
+    catch(Exception e){
+      return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
