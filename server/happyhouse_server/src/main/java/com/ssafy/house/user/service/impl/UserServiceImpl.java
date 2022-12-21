0e28655c467b7d072fea7d893454a8a0d0d21a48
김호균 <H0KyunGim@gmail.com>
2022-12-21 13:01:55 +0900

    feature/52-뉴스크롤링백엔드 (#59)
    * fix: 프리티어
    
    * fix: warn
    
    * feat: 뉴스크롤링백엔드

--- server/happyhouse_server/src/main/java/com/ssafy/house/user/service/impl/UserServiceImpl.java
+++ server/happyhouse_server/src/main/java/com/ssafy/house/user/service/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.house.config.security.JWTProvider;
