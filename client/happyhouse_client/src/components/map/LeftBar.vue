6faae8b00aa2e1734f96396dd9df0243eda46edb
Jahwi <58069290+Jahwi-0413@users.noreply.github.com>
2022-12-23 14:14:36 +0900

    feature : 지도 기본 화면 (#71)

---
+++ client/happyhouse_client/src/components/map/LeftBar.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-container>
+    <v-row>
+      <div class="search-bar py-4 pl-4 d-flex flex-column">
+        <div class="d-flex flex-row flex-wrap">
+          <span>
+            <v-chip
+              v-for="(keyword, index) in keywordList"
+              :key="index"
+              class="mr-2"
+              close
+              @click:close="deleteKeyword(index)"
+            >
+              {{ keyword }}
+            </v-chip>
+          </span>
+          <div class="d-flex flex-row full-width">
+            <input
+              v-model="keyword"
+              class="ml-3 input"
+              type="text"
+              @keydown.enter="addKeyword"
+            />
+            <v-btn text class="mr-3" @click="search">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </div>
+        </div>
+      </div>
+    </v-row>
+    <v-row>
+      <trade-search-list :tradeList="dummyList"></trade-search-list>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import TradeSearchList from "./TradeSearchList";
+export default {
+  data() {
+    return {
+      dummyList: [
+        {
+          aptName: "apt1",
+          tradeDate: "2022년 00월 00일",
+          area: "00",
+          tradePrice: "----원",
+          address: "00시 00구 00동",
+          addressCode: "00000000",
+        },
+        {
+          aptName: "apt2",
+          tradeDate: "2022년 00월 00일",
+          area: "00",
+          tradePrice: "----원",
+          address: "00시 00구 00동",
+          addressCode: "00000000",
+        },
+        {
+          aptName: "apt3",
+          tradeDate: "2022년 00월 00일",
+          area: "00",
+          tradePrice: "----원",
+          address: "00시 00구 00동",
+          addressCode: "00000000",
+        },
+        {
+          aptName: "apt4",
+          tradeDate: "2022년 00월 00일",
+          area: "00",
+          tradePrice: "----원",
+          address: "00시 00구 00동",
+          addressCode: "00000000",
+        },
+      ],
+      keywordList: [],
+      keyword: "",
+    };
+  },
+  components: {
+    TradeSearchList,
+  },
+  methods: {
+    search() {
+      if (this.keyword !== "") {
+        this.keywordList.push(this.keyword);
+      }
+    },
+    addKeyword() {
+      this.keywordList.push(this.keyword);
+      this.keyword = "";
+    },
+    deleteKeyword(index) {
+      this.keywordList.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: fixed;
+  left: 0;
+  width: 400px;
+  height: 100vh;
+}
+
+.search-bar {
+  border: 1px solid #9e9e9e;
+  border-left: none;
+  border-right: none;
+  width: 100%;
+}
+.input {
+  font-size: 1.3rem;
+  width: 75%;
+  border: none;
+}
+.input:focus {
+  outline: none;
+}
+
+.full-width {
+  width: 100%;
+}
+</style>
--- client/happyhouse_client/src/views/MapView.vue
+++ client/happyhouse_client/src/views/MapView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="margin-top">
+    <left-bar></left-bar>
+    <the-kakao-map></the-kakao-map>
   </div>
 </template>
+
+<script>
+import LeftBar from "@/components/map/LeftBar";
+import TheKakaoMap from "@/components/map/TheKakaoMap";
+
+export default {
+  components: {
+    LeftBar,
+    TheKakaoMap,
+  },
+};
+</script>
+
+<style scoped>
+.margin-top {
+  margin-top: -5.5rem;
+}
+</style>
