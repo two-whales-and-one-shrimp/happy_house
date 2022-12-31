b41573d94990fbf9e6c187a49a6870c7d28b9ebc
jaehe <jaehe0413@naver.com>
2022-12-31 17:28:11 +0900

    eslint undef off, add map control

--- client/happyhouse_client/src/components/map/TheKakaoMap.vue
+++ client/happyhouse_client/src/components/map/TheKakaoMap.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+/* global kakao */
+/* eslint no-undef: "off" */
 export default {
   props: {
     aptList: Array,
@@ -19,7 +21,6 @@ export default {
       return;
     }
     const script = document.createElement("script");
-    // eslint-disable-next-line no-undef
     script.onload = () => kakao.maps.load(this.initMap);
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${process.env.VUE_APP_KAKAO_MAP_KEY}&libraries=services`;
     document.head.appendChild(script);
@@ -28,12 +29,13 @@ export default {
     initMap() {
       const container = document.getElementById("map");
       const options = {
-        // eslint-disable-next-line no-undef
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 5,
       };
-      // eslint-disable-next-line no-undef
       this.map = new kakao.maps.Map(container, options);
+
+      const zoomControl = new kakao.maps.ZoomControl();
+      this.map.addControl(zoomControl, kakao.maps.ControlPosition.TOPRIGHT);
     },
     makeMarkers() {
       //기존 마커 다 지우고 시작
@@ -41,17 +43,14 @@ export default {
 
       //dummydata
       for (const apt of this.aptList) {
-        // eslint-disable-next-line no-undef
         const markerPosition = new kakao.maps.LatLng(
           apt.latlngObj.y,
           apt.latlngObj.x
         );
-        // eslint-disable-next-line no-undef
         const marker = new kakao.maps.Marker({
           position: markerPosition,
         });
         var iwContent = `<div style="padding:3px; text-align:center">${apt.aptName}</div>`;
-        // eslint-disable-next-line no-undef
         const infowindow = new kakao.maps.InfoWindow({
           position: markerPosition,
           content: iwContent,
@@ -70,15 +69,12 @@ export default {
     },
     //지도 중심 바꾸기
     async changeMapCenter() {
-      // eslint-disable-next-line no-undef
       let geocoder = new kakao.maps.services.Geocoder();
       let coords = null;
 
       if (this.centerAddress.x == undefined) {
         geocoder.addressSearch(this.centerAddress, (result, status) => {
-          // eslint-disable-next-line no-undef
           if (status === kakao.maps.services.Status.OK) {
-            // eslint-disable-next-line no-undef
             coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
             //지도 중심 이동
@@ -87,7 +83,6 @@ export default {
           }
         });
       } else {
-        // eslint-disable-next-line no-undef
         coords = new kakao.maps.LatLng(
           this.centerAddress.y,
           this.centerAddress.x
@@ -117,7 +112,7 @@ export default {
 #map {
   position: fixed;
   left: 400px;
-  width: 80vw;
+  width: 78.3vw;
   height: 90vh;
   border-left: 1px solid #9e9e9e;
   border-top: 1px solid #9e9e9e;
